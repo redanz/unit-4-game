68e6c6a41a3821d6ff573bf6ac2ad6376eb6c3d6
Redanz <nader.zargham@gmail.com>
2019-01-27 01:55:40 -0800

    improved css

--- javascript/game.js
+++ javascript/game.js
@@ -1,39 +1,11 @@
-// * There will be four crystals displayed as buttons on the page.
-
-//    * The player will be shown a random number at the start of the game.
-
-//    * When the player clicks on a crystal, it will add a specific amount of points 
-// 		to the player's total score. 
-
-//      * Your game will hide this amount until the player clicks a crystal.
-//      * When they do click one, update the player's score counter.
-
-//    * The player wins if their total score matches the random number from the 
-// 		beginning of the game.
-
-//    * The player loses if their score goes above the random number.
-
-//    * The game restarts whenever the player wins or loses.
-
-//    * When the game begins again, the player should see a new random number. 
-// 		Also, all the crystals will have four new hidden values. Of course, the user's 
-// 		score (and score counter) will reset to zero.
-
-//    * The app should show the number of games the player wins and loses. 
-// 		To that end, do not refresh the page as a means to restart the game.
-
-// ##### Option 1 Game design notes
-
-// * The random number shown at the start of the game should be between 19 - 120.
-// * Each crystal should have a random hidden value between 1 - 12.
-
-var score = 0; //should be hidden in the beginning
+var score = 0;
 var wins = 0;
-var loses = 0;
+var losses = 0;
 var randomNum = 0;
 var clicks = 0;
 var record = 'n/a';
 var currentClicks = 0;
+$('#round-result').text('Click Crystal to Play')
 
 var crystals = [
 	{
@@ -78,9 +50,8 @@ function updateNumbers(){
 	$('#record').text(record);
 	$('#score').text(0);
 	$('#wins').text(wins);
-	$('#loses').text(loses);
+	$('#losses').text(losses);
 	$('#randomNum').text(randomNum);
-	console.log([randomNum, crystals[0].value, crystals[1].value, crystals[2].value, crystals[3].value])
 }
 
 
@@ -88,7 +59,7 @@ updateNumbers();
 
 $('img').on('click', function(){
 
-	$('#result').text('');
+	$('#round-result').text('');
 	for (i=0; i<crystals.length; i++){
 		if($(this).attr('id') == crystals[i].color) {
 			updateScore();
@@ -99,22 +70,21 @@ $('img').on('click', function(){
 		
 		wins++;
 		score = 0;
-		$('#result').text('You won!');
+		$('#round-result').text('You won!');
 		
 		if (typeof(record)=='string' || currentClicks < record){
-			$('#result').text('You won! New record!');
+			$('#round-result').text('New record!');
 			record = currentClicks;
 		}
 		updateNumbers();
 
 	} else if (score > randomNum) {
 		
-		loses++;
+		losses++;
 		score = 0;
-		$('#result').text('You lost!');
+		$('#round-result').text('You lost!');
 		updateNumbers();
 	}
-
 
 })
 
